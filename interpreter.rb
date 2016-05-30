db7fc59a758ad34e154c49fe104c470498a345f7
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2016-05-31 00:34:01 +0800

    Interpret method complete

--- interpreter.rb
+++ interpreter.rb
@@ -72,6 +72,133 @@ class HRMInterpreter
     line.strip.split(" ").each { |val| @inputs.push(get_raw_val(val)) }
   end
 
+  def interpret
+    steps = 0
+    print_log("interpreting...")
+    inb = Regexp.new(/INBOX/)
+    oub = Regexp.new(/OUTBOX/)
+    eof = Regexp.new(/END/)
+    sub = Regexp.new(/SUB\s+\[(?<addr>\d+)\]|SUB\s+(?<index>\d+)/)
+    add = Regexp.new(/ADD\s+\[(?<addr>\d+)\]|ADD\s+(?<index>\d+)/)
+    jpz = Regexp.new(/JUMPZ\s+(?<label>\w+)/)
+    jpn = Regexp.new(/JUMPN\s+(?<label>\w+)/)
+    jmp = Regexp.new(/JUMP\s+(?<label>\w+)/)
+    bup = Regexp.new(/BUMPUP\s+\[(?<addr>\d+)\]|BUMPUP\s+(?<index>\d+)/)
+    bdn = Regexp.new(/BUMPDN\s+\[(?<addr>\d+)\]|BUMPDN\s+(?<index>\d+)/)
+    cpt = Regexp.new(/COPYTO\s+\[(?<addr>\d+)\]|COPYTO\s+(?<index>\d+)/)
+    cpf = Regexp.new(/COPYFROM\s+\[(?<addr>\d+)\]|COPYFROM\s+(?<index>\d+)/)
+    lbl = Regexp.new(/(?<label>\w+):/)
+    ptr = 0
+    x = nil
+    finished = false
+    while !finished && ptr >= 0 && ptr < commands.length do
+      print_log(format("mem: %s", @mem))
+      cmd = @commands[ptr]
+      print_log(format("interpreting commands[%d]: %s", ptr, cmd))
+      if cmd.match(inb)
+        if @inputs.empty?
+          finished = true
+        else
+          x = inputs.delete_at(0)
+        end
+      elsif cmd.match(oub)
+        @outputs.push(x)
+        x = nil
+      elsif cmd.match(add)
+        m = cmd.match(add)
+        if !m['addr'].nil?
+          addr = m['addr']
+          i = get_val_from_mem(addr)
+          x = add_raw(x, get_val_from_mem(i))
+        else
+          i = m['index']
+          x = add_raw(x, get_val_from_mem(i))
+        end
+      elsif cmd.match(sub)
+        m = cmd.match(sub)
+        if !m['addr'].nil?
+          addr = m['addr']
+          i = get_val_from_mem(addr)
+          x = sub_raw(x, get_val_from_mem(i))
+        else
+          i = m['index']
+          x = sub_raw(x, get_val_from_mem(i))
+        end
+      elsif cmd.match(jpz)
+        unless !cmp_raw(x, "eq", 0)
+          m = cmd.match(jpz)
+          label = m['label']
+          ptr = @labels[label]
+        end
+      elsif cmd.match(jpn)
+        unless !cmp_raw(x, "lt", 0)
+          m = cmd.match(jpn)
+          label = m['label']
+          ptr = @labels[label]
+        end
+      elsif cmd.match(jmp)
+        m = cmd.match(jmp)
+        label = m['label']
+        ptr = @labels[label]
+      elsif cmd.match(bup)
+        m = cmd.match(bup)
+        if !m['addr'].nil?
+          addr = m['addr']
+          i = get_val_from_mem(addr)
+          set_val_to_mem(i, add_raw(get_val_from_mem(i), 1))
+          x = get_val_from_mem(i)
+        else
+          i = m['index']
+          set_val_to_mem(i, add_raw(get_val_from_mem(i), 1))
+          x = get_val_from_mem(i)
+        end
+      elsif cmd.match(bdn)
+        m = cmd.match(bdn)
+        if !m['addr'].nil?
+          addr = m['addr']
+          i = get_val_from_mem(addr)
+          set_val_to_mem(i, sub_raw(get_val_from_mem(i), 1))
+          x = get_val_from_mem(i)
+        else
+          i = m['index']
+          set_val_to_mem(i, sub_raw(get_val_from_mem(i), 1))
+          x = get_val_from_mem(i)
+        end
+      elsif cmd.match(cpt)
+        m = cmd.match(cpt)
+        if !m['addr'].nil?
+          addr = m['addr']
+          i = get_val_from_mem(addr)
+          set_val_to_mem(i, x)
+        else
+          i = m['index']
+          set_val_to_mem(i, x)
+        end
+      elsif cmd.match(cpf)
+        m = cmd.match(cpf)
+        if !m['addr'].nil?
+          addr = m['addr']
+          i = get_val_from_mem(addr)
+          x = get_val_from_mem(i)
+        else
+          i = m['index']
+          x = get_val_from_mem(i)
+        end
+      elsif cmd.match(eof)
+        finished = true
+      elsif cmd.match(lbl)
+        # label
+      else
+        print_warn(format("Ignore command %s", cmd))
+      end
+      ptr += 1
+      steps += 1
+      print_log(format("ptr: %d", ptr))
+    end
+    print_log(format("interpreted in %d steps with %d commands",
+                      steps, @commands.length))
+  end
+
   def add_raw(left, right)
     if left.instance_of?(String) && right.instance_of?(String)
       print_error(format("Unable to add values of non-integer types"))
@@ -256,5 +383,11 @@ if __FILE__ == $0
   parser.parse!
   interpreter = HRMInterpreter.new(parser)
 
+  puts "Before:"
+  print interpreter.to_s
+
+  interpreter.interpret
+
+  puts "After:"
   print interpreter.to_s
 end 
